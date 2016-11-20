0b37f97f2c243479f543c9fd6a0519d644d5c095
Ivan Trendafilov <igtrendafi@gmail.com>
2016-11-20 16:01:51 +0200

    initial app

---
+++ stock_app_ivantrendafilov/src/main/java/stock_app_ivantrendafilov/stock_app_ivantrendafilov/App.java
@@ -0,0 +1,13 @@
+package stock_app_ivantrendafilov.stock_app_ivantrendafilov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
